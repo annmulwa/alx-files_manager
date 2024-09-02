f2e140f711944debc91090da933c2deaaa8a0d3e
annmulwa <annmulwa2000@gmail.com>
2024-09-02 12:25:47 +0300

    files manager

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,7 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
+// class that defines methods for commonly used redis commands
 class RedisClient {
   constructor() {
     this.client = createClient();
@@ -9,6 +10,7 @@ class RedisClient {
     });
   }
 
+  // check connection status and report
   isAlive() {
     if (this.client.connected) {
       return true;
@@ -16,18 +18,21 @@ class RedisClient {
     return false;
   }
 
+  // get the value for a given key from redis server
   async get(key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const value = await redisGet(key);
     return value;
   }
 
+  // set the value for a given key from redis server
   async set(key, value, time) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     await this.client.expire(key, time);
   }
 
+  // delete the value for a given key from redis server
   async del(key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
